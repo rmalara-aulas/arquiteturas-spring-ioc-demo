470e55dce09c3e9518e3872d624e59e72a99510c
rodrigo <rodrigo@gmxcheckout.com.br>
2024-11-14 20:48:12 -0300

    Init

---
+++ src/main/java/com/example/demo/ClientDAOMemoria.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class ClientDAOMemoria implements ClienteDAO{
+
+    private static Map<Integer, Cliente> dados;
+
+    static {
+        dados = new HashMap<>();
+    }
+    @Override
+    public void salvaCliente(Cliente cli) {
+        dados.put(cli.getId(), cli);
+    }
+}
---
+++ src/main/java/com/example/demo/Cliente.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import lombok.Data;
+
+@Data
+public class Cliente {
+    private Integer id;
+    private String nome;
+}
